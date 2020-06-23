43f7b68a0af0b85c529694e8d55624ab8937f121
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2020-06-23 22:02:56 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -98,4 +98,14 @@ class TicTacToe
     won? && board[won? [0]]
   end
   
+  def play
+    until over?
+      turn
+    end
+    if won?
+      puts "Congratulations #{winner}!"
+    else
+      puts "Cat's Game!"
+    end
+  end
 end
