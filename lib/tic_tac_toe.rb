038df9ff06fc624b16f67dd3385ca9135614729d
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2020-06-23 22:13:17 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -56,71 +56,43 @@ class TicTacToe
   end
   
   def turn 
-    # ask for input
+    
     puts "Please make a move by entering a number between 1-9"
-    # get input
     input = gets.strip
-    # translate input into index
     index = input_to_index(input)
-    # if index is valid
-    #   make the move for index
-    #   show the board
-    # else
-    #   ask for input again
-    # end
-    if valid_move?(index)
+    
+   if valid_move?(index)
       move(index, current_player)
       display_board
     else
       turn
     end
-    # index = -1
-    # until valid_move?(index)
-    #   puts "Please make a move by entering a number between 1-9"
-    #   input = gets.strip 
-    #   index = input_to_index(input)
-    #   if valid_move?(index)
-    #     move(index, current_player)
-    #     display_board
-    #   else
-    #     puts "whoops!"
-    #   end
-    # end
+    
   end
 
   def won? 
     WIN_COMBINATIONS.find do |win_combo|
-      # will be the integer index values for the win combination we're currently checking
+      
       position_1_index = win_combo[0]
       position_2_index = win_combo[1]
       position_3_index = win_combo[2]
 
-      # each of these will be "X", "O" or " "
+      
       position_1_token = board[position_1_index]
       position_2_token = board[position_2_index]
       position_3_token = board[position_3_index]
 
       position_1_token == position_2_token && position_2_token == position_3_token && position_taken?(position_1_index)
-      # board[win_combo[0]] == board[win_combo[1]] && board[win_combo[1]] == board[win_combo[2]] && position_taken?(win_combo[0])
+      
     end
   end
 
   def full? 
-    # board.all? do |token|
-    #   ["X","O"].include?(token)
-    # end
+    
     board.all? do |token|
       token == "X" || token == "O"
     end
-    # (0..8).all? do |index|
-    #   position_taken?(index)
-    # end
-    # board.all? do |token|
-    #   token != " "
-    # end
-    # board.none? do |token|
-    #   token == " "
-    # end
+    
   end
 
   def draw?
